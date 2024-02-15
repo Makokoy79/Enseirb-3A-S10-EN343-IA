bcfed0529d7626da3a2028e3c5aeb2c19e2b9f0a
LilianT11 <lilian.brunet.lb@gmail.com>
2024-02-15 09:02:06 +0100

    files for implement couche en c

--- codeC/Inference/main.c
+++ codeC/Inference/main.c
@@ -20,3 +20,16 @@ int main(int argc, char* argv[]){
   printf("Au travail!\n");
   return 0;
 }
+
+
+//  Fonction pour récupérer tout les paramètres du réseau
+//  Pour chaque couche : récupérer les poids et biais à parti des ".txt"
+//  Faire des malloc pour stocker les donées
+
+//  Application de chaque couche à partir  des paramètres
+//    Ecriture des fonctions correspondant à chaque couche
+//      Multiplication, Somme       => Convolution
+//      Comparaison                 => Max Pooling
+//       Fonction d'activation
+//       Mise à plat                => Flatten
+//    Ecriture des fonctions de comparaison de chaque couche afin de vérifier le résultat
